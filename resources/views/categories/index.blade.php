8ff9fafe492f35c7a3a3f9c5696eafbce7d84218
Kestas <kestutisjankevicius9@gmail.com>
2023-10-15 14:10:01 +0300

    Categories index

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,9 +1,25 @@
 <x-layout>
-<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
-    <h3>Test</h3>
+<div class="row justify-content-center">
+    <div class="col-auto">
+        <table class="w-full table-auto rounded-sm">
+            <tbody>
+                @foreach($categories as $category)
+                <tr class="border-gray-300">
+                    <td>
+                        <h2> {{ $category->name }} </h2>
+                    </td>
+                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                       <form method="POST" action="/categories/{{$category->id}}">
+                        @csrf
+                        @method('DELETE')
+                        <button class="text-red-500"><i class="fa-solid fa-trash"></i> Ištrinti</button>
+                       </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
 
-    {{-- @foreach($categories as $category) --}}
-    
-
+    </div>
 </div>
 </x-layout>
